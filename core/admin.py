bfc6cfaad6c7e52f0f04f98b96b118c0f8de73b3
congdoan6997 <congdoan6997@gmail.com>
2019-12-30 23:32:16 +0300

    test 1

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Item, Oder, OderItem
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'discount_price', 'category', 'label']
+    list_filter = ['category']
+    search_fields = ['title']
+
 
 # Register your models here.
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Oder)
+admin.site.register(OderItem)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Item
+from django.views.generic import ListView, DetailView
 # Create your views here.
+
+
+def index(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = 'home.html'
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product.html'
